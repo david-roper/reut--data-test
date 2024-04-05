450aec4ac845c99e3d960fa823ee9973b8be77cf
David Roper <davidroper321@gmail.com>
2024-04-05 15:10:36 -0400

    filter and concat df to remove names

--- polarsTest.py
+++ polarsTest.py
@@ -1,6 +1,7 @@
 import os
 import polars as pl
 import polars.selectors as cs
+import pandas as pd
 
 df = pl.read_excel('data/raw_morning_data.xlsx')
 
@@ -13,27 +14,33 @@ df2 = df.slice(0,1)
 
 #print(df)
 
-# df = df.slice(1) #removes top row
-#print(df2)
+df = df.slice(1) #removes top row
+print('here is df2 \n')
+print(df2)
 # df = df.rename({"Please enter the participant number that was sent to you here so that we can identify you:":"participant ID"})
 # df2 = df2.rename({"Please enter the participant number that was sent to you here so that we can identify you:":"participant ID"})
 df= df.sort(["participant ID", "What was yesterday&apos;s date? (i.e. the date of your bedtime)"])
 
 #convert to pandas for name filtering
 df = df.to_pandas()
-print(df.iloc[:,6])
+
 df.loc[df['participant ID'].str.contains('[a-zA-Z]'), 'participant ID'] = ''
 
 for i,col in enumerate(df.columns):
     if (col == ''):
         df.rename(columns={col: f"column_{i}"}, inplace=True)
 
-print(df['column_6'] + '\n')
+
 print('\nnow test concat')
+df2 = df2.to_pandas()
+
+df = pd.concat([df2,df], ignore_index=True)
+print(df)
+
+
 #convert back into polar df
 df = pl.from_pandas(df)
-
-#df = pl.concat([df,df2])
 print(df)
+
 path = os.curdir + '/data/sorted-data.csv'
 df.write_csv(path,separator=',')
