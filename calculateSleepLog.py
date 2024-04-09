da5e4923be1a427ea004733d9cf61fe571cc364d
david-roper <davidroper321@gmail.com>
2024-04-08 20:10:05 -0400

    rename empty column

--- calculateSleepLog.py
+++ calculateSleepLog.py
@@ -1,11 +1,14 @@
 import polars as pl
 import pandas as pd
 
-df = pl.read_excel('data/formattedMorning.xlsx')
+df = pl.read_excel('data/formattedMorningData.xlsx')
 
 df = df.slice(1)
 
 print(df)
+
+df.rename({'':"participant ID"})
+
 
 '''
 convert calculations into python code, iterate through each participants morning and evening weekly data. take those weeks of data and produce the spss lines through calculations
