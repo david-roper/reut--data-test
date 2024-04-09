b284744b2b1f284a020c0da2a6f051bd1b2d40b4
David Roper <davidroper321@gmail.com>
2024-04-09 15:51:09 -0400

    date conversion can now be  used on df column

--- calculateSleepLog.py
+++ calculateSleepLog.py
@@ -5,7 +5,6 @@ import pandas as pd
 def date_converter(date):
     dates = {"01":"Jan","02":"Feb","03":"Mar","04":"Apr","05":"May","06":"June","07":"July","08":"Aug","09":"Sept", "10":"Oct","11":"Nov", "12":"Dec"}
 
-    print(date[3:5])
     date = date[:3] + dates[date[3:5]] + date[5:]
 
     return date
@@ -18,12 +17,10 @@ df2 = pl.read_excel('data/formattedEvening.xlsx')
 df = df.slice(1)
 
 
-s = "27/03/2023"
+df = df.with_columns(pl.col("What is today's date? (DD/MM/YYYY)").apply(date_converter))
 
 print(df)
 print(df2)
-
-print(date_converter(s))
 
 
 '''
